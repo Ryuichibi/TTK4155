14b910dd924cd8115bd615b2ae4c45359ec648a2
Ole Sværen <ole.svaeren@gmail.com>
2025-09-24 00:14:05 +0200

    changed the filestructure

---
+++ lib/analog.h
@@ -0,0 +1,60 @@
+#include <stdint.h>
+#define F_CPU 4915200
+#include <util/delay.h>
+#include <avr/io.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct analog_input{
+  uint8_t analog_ch0;
+  uint8_t analog_ch1;
+  uint8_t analog_ch2;
+  uint8_t analog_ch3;
+}analog_input;
+
+typedef struct calib_parameters {
+  uint8_t x_neutral_max;
+  uint8_t x_neutral_min;
+  uint8_t x_max;
+  uint8_t x_min;
+  uint8_t y_neutral_max;
+  uint8_t y_neutral_min;
+  uint8_t y_max;
+  uint8_t y_min;
+} calib_parameters;
+
+
+void analog_init(void);
+analog_input analog_read();
+
+enum directionX{
+  NEUTRALX,
+  RIGHT,
+  LEFT
+};
+
+enum directionY{
+  NEUTRALY,
+  FORWARD,
+  BACKWARD
+};
+void joystick_calibrate(calib_parameters*);
+
+typedef struct joystick {
+  int8_t x_pos;
+  int8_t y_pos;
+  
+  bool button;
+  enum directionY direction_y;
+  enum directionX direction_x;
+
+}joystick;
+
+joystick joystick_read(analog_input analog_in, calib_parameters parameters);
+
+typedef struct touchpad {
+  uint8_t x_pos;
+  uint8_t y_pos;
+}touchpad;
+
+touchpad touchpad_read();//TODO takes in the wrong inputs compared to the function in .c
---
+++ lib/uart.c
@@ -0,0 +1,36 @@
+#define F_CPU 4915200
+#define UBRR 31 //using 2400 as baudrate, we get this for the registers according to the formula in the datasheet
+#include "uart.h"
+
+#define BIT_TIME_US (1000000 / BAUDRATE)
+
+
+int uart_send_byte(char data,FILE* file){
+    //Wait until the serial port is ready for sending something new
+    while (!(UCSR0A & (1<<UDRE0)))
+    ;
+    //load new data into sending buffer
+    UDR0 = data;
+    return 0;
+}
+
+int uart_receive_byte (FILE* file){
+    while (!(UCSR0A & (1<<RXC0)))
+    ;
+    //load new data into sending buffer
+    return UDR0;
+}
+
+FILE* uart_init(int baud){
+    //setting the baudrate:
+    UBRR0H = (UBRR >> 8);
+    UBRR0L = UBRR;
+    
+    //enable RX0 and TX0:
+    UCSR0B = (1<<RXEN0) | (1<<TXEN0);
+
+    //set frame format.
+    //it is currently set to 8 databits, 1 stop bit and none in parity bit
+    UCSR0C = (1<<URSEL0) | (3<<UCSZ00);
+    return fdevopen(uart_send_byte, uart_receive_byte);
+}
