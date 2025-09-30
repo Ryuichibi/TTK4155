8d85cd09a1116ceff086727ad731e3ee4f466789
OleSvaren <ole.svaeren@gmail.com>
2025-09-30 12:36:15 +0200

    fixed another conflict

--- lib/oled.c
+++ lib/oled.c
@@ -7,7 +7,6 @@ void oled_init()
     // Set D/C as output
     DDRB |= (1 << COMMAND_DATA);
 
-<<<<<<< HEAD
     oled_write_command(DISPLAY_OFF);
     oled_write_command(SET_SEGMENT_REMAP);
     oled_write_command(SET_COM_PINS);
@@ -30,52 +29,16 @@ void oled_init()
     oled_write_command(DISPLAY_RESUME_RAM_CONTENT);
     oled_write_command(SET_DISPLAY_NOT_INVERTED);
     oled_write_command(DISPLAY_ON_NORMAL_MODE);
-=======
-    spi_send_char(0xae, SLAVE_SELECT);
-    spi_send_char(0xa0, SLAVE_SELECT);
-    spi_send_char(0xda, SLAVE_SELECT);
-    spi_send_char(0x12, SLAVE_SELECT);
-    spi_send_char(0xc0, SLAVE_SELECT);
-    spi_send_char(0xa8, SLAVE_SELECT);
-    spi_send_char(0x3f, SLAVE_SELECT);
-    spi_send_char(0xd5, SLAVE_SELECT);
-    spi_send_char(0x80, SLAVE_SELECT);
-    spi_send_char(0x81, SLAVE_SELECT);
-    spi_send_char(0xF0, SLAVE_SELECT);
-    spi_send_char(0xd9, SLAVE_SELECT);
-    spi_send_char(0x21, SLAVE_SELECT);
-    spi_send_char(0x20, SLAVE_SELECT);
-    spi_send_char(0x02, SLAVE_SELECT);
-    spi_send_char(0xdb, SLAVE_SELECT);
-    spi_send_char(0x30, SLAVE_SELECT);
-    spi_send_char(0xad, SLAVE_SELECT);
-    spi_send_char(0x00, SLAVE_SELECT);
-    spi_send_char(0xa4, SLAVE_SELECT);
-    spi_send_char(0xa6, SLAVE_SELECT);
-    //spi_send_char(0xb4, SLAVE_SELECT); // Select page
-    //spi_send_char(0x21, SLAVE_SELECT);
-    //spi_send_char(0x00, SLAVE_SELECT);
-    //spi_send_char(0x7f, SLAVE_SELECT);
-    spi_send_char(0xaf, SLAVE_SELECT);
->>>>>>> a2a85f8ecbc024e6d0803ce030328cba76b96425
 }
 
 void oled_reset()
 {
-<<<<<<< HEAD
-    for (uint8_t i = 0; i < 8; i++) {
-        oled_clear_row(i);
-    }
-
-    // TODO: Do more?
-=======
     for (uint8_t i = 0; i < 8; i++)
     {
         oled_clear_row(i);
     }
 
     //TODO: Do more?
->>>>>>> a2a85f8ecbc024e6d0803ce030328cba76b96425
 }
 
 // void oled_home()
@@ -83,46 +46,8 @@ void oled_reset()
 
 // }
 
-void oled_goto_row(uint8_t row)
-<<<<<<< HEAD
-=======
-{
-    PORTB &= ~(1 << COMMAND_DATA);
 
-    uint8_t command = 0xb0;
-    command |= (row & 0x0f);
-    spi_send_char(command, SLAVE_SELECT);
-}
 
-void oled_goto_column(uint8_t column)
-{
-    PORTB &= ~(1 << COMMAND_DATA);
-
-    uint8_t lower_column_command = 0x00;
-    uint8_t upper_column_command = 0x10;
-
-    lower_column_command |= (column & 0x0f);
-    upper_column_command |= ((column & 0xf0) >> 4);
-
-    spi_send_char(lower_column_command, SLAVE_SELECT);
-    spi_send_char(upper_column_command, SLAVE_SELECT);
-
-}
-
-void oled_clear_row(uint8_t row)
-{
-    oled_goto_row(row);
-
-    PORTB |= (1 << COMMAND_DATA);
-
-    for (uint8_t i = 0; i < 128; i++)
-    {
-        spi_send_char(0x00, SLAVE_SELECT);
-    }
-
-    PORTB &= ~(1 << COMMAND_DATA);
-
-}
 
 void oled_position(uint8_t row, uint8_t column)
 {
@@ -131,8 +56,7 @@ void oled_position(uint8_t row, uint8_t column)
 
 }
 
-void oled_write_data(char data)
->>>>>>> a2a85f8ecbc024e6d0803ce030328cba76b96425
+void oled_goto_row(uint8_t row)
 {
     char row_char = SET_PAGE | row;
     oled_write_command(row_char);
@@ -158,27 +82,19 @@ void oled_clear_row(uint8_t row)
 }
 
 
-
-void oled_position(uint8_t row, uint8_t column)
-{
-    oled_goto_row(row);
-    oled_goto_column(column);
-}
-
-void oled_write_data(volatile char data)
+void oled_write_data(char data)
 {
     PORTB |= (1 << COMMAND_DATA);
     spi_send_char(data, SLAVE_SELECT);
     PORTB &= ~(1 << COMMAND_DATA);
 }
 
-void oled_write_command(volatile char command)
+void oled_write_command(char command)
 {
     PORTB &= ~(1 << COMMAND_DATA);
     spi_send_char(command, SLAVE_SELECT);
 }
 
-<<<<<<< HEAD
 void oled_print_letter(char data)
 {
     for (uint8_t i = 0; i < 8; i++) {
@@ -188,17 +104,9 @@ void oled_print_letter(char data)
                            // that looks like an alphanumerical character
         oled_write_data(encoded_data);
     }
-=======
-// }
+ }
 
-void oled_set_brightness(uint8_t level)
-{
-    PORTB &= ~(1 << COMMAND_DATA);
 
-    spi_send_char(0x81, SLAVE_SELECT);
-    spi_send_char(level, SLAVE_SELECT);
->>>>>>> a2a85f8ecbc024e6d0803ce030328cba76b96425
-}
 
 void oled_set_brightness(uint8_t level)
 {
--- src/main.c
+++ src/main.c
@@ -29,80 +29,17 @@ int main()
            calibration_values.x_min, calibration_values.y_max,
            calibration_values.x_max);
 
-<<<<<<< HEAD
     spi_init();
     oled_init();
-=======
-  analog_init();
-  calib_parameters calibration_values = {0, -1, 0, -1, 0, -1, 0, -1};
-  //x_max = left, x_min = right, y_max = down, y_min = up
-  //joystick_calibrate(&calibration_values);
-  printf("y_min: %d, xmin: %d, ymax: %d,x_max %d",calibration_values.y_min, calibration_values.x_min, calibration_values.y_max, calibration_values.x_max);
 
-  spi_init();
-  oled_init();
-  oled_goto_row(0x02);
-  oled_goto_column(0x40);
+    spi_init();
+    oled_init();
+    oled_goto_row(0x02);
+    oled_goto_column(0x40);
 
-  while (1) {
-    //analog_data = analog_read();
-    //joystick_1 = joystick_read(analog_data, calibration_values);
-
-    // oled_write_data(0b01000001);
-    // oled_write_data(0b01111111);
-    // oled_write_data(0b01111111);
-    // oled_write_data(0b01001001);
-    // oled_write_data(0b00011101);
-    // oled_write_data(0b00000001);
-    // oled_write_data(0b00000011);
-    // oled_write_data(0b00000000);
-
-    oled_write_data(0xff);
-    spi_send_char(0xaf, PB4);
-
-    // spi_send_char(0xa5, PB4);
-    // _delay_ms(2000);
-    // spi_send_char(0xa4, PB4);
-    // _delay_ms(2000);
-
-
-
-
-    //printf("%d %d\n", analog_data.analog_ch2, analog_data.analog_ch3);
-    //if (joystick_1.direction_x == RIGHT) printf("Right\n");
-    //if (joystick_1.direction_x == LEFT) printf("Left\n");
-    //if (joystick_1.direction_y == FORWARD) printf("Forward\n");
-    //if (joystick_1.direction_y == BACKWARD) printf("Backward\n");
-    //if (joystick_1.direction_x == NEUTRALX) printf("Neutral_x\n");
-    //if (joystick_1.direction_y == NEUTRALY) printf("Neutral_y\n");
-    //printf("x direction: %d \n y direction: %d", joystick_1.x_pos, joystick_1.y_pos);
-  //  printf("y:%d, x:%d\n", analog_data.analog_ch0,analog_data.analog_ch1);
-  }
-
-  //data = (unsigned char) uart_receive_byte(UART);
-  //printf("Yes");
-  //sram_write(10, 10);
-  
-
-  // DDRA |= (1<< PA0);
-  // while (1){
-  //   printf("Hello, %d", 12);
-  //   _delay_ms(1000);
-  //   data = (unsigned char) uart_receive_byte(UART);
-  //   if (data == 'T') PORTA ^= (1<<PA0);
-  // }
-
-
-	return 0;
->>>>>>> a2a85f8ecbc024e6d0803ce030328cba76b96425
-
-    char letter = 'a';
-    uint8_t start_col = 0;
-    oled_clear_row(7);
     while (1) {
-        oled_goto_row(3);
-        oled_goto_column(54);
-        oled_print_letter(letter);
+        oled_write_data(0xff);
+        spi_send_char(0xaf, PB4);
     }
     return 0;
 }
