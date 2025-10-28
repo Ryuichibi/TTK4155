a411794c9a67160febbd46e4938babc2bf9d0e54
Markus Johansen <markusbjoh@gmail.com>
2025-10-28 15:07:19 +0100

    Working servo

--- node2/main.c
+++ node2/main.c
@@ -18,21 +18,29 @@
 #define F_CPU 84000000
 #define F_CAN F_CPU/2
 
+#define CDTY_L 0b1100001011111000u // This is 1ms
+#define CDTY_H 0b1011100001101100u // This is 2ms 
+#define CDTY_STEP_DIFF 0b11011u
+
 int main()
 {
     SystemInit();
 
     WDT->WDT_MR = WDT_MR_WDDIS; //Disable Watchdog Timer
 
-    //PIO->PIO_EN 
-    PMC->PMC_PCER1 |= (1 << 4);
     PIOB->PIO_PDR |= PIO_PDR_P13;
-    PIOB->PIO_ABSR|= PIO_ABSR_P13;
+    PIOB->PIO_MDDR |= PIO_MDDR_P13;
+    PIOB->PIO_ABSR |= PIO_ABSR_P13;
 
+    PMC->PMC_PCER1 |= (1 << 4);
     PWM->PWM_CLK |= 1;
-    PWM->PWM_CH_NUM[1].PWM_CPRD = 0b00000000000110011010001010000000;
-    PWM->PWM_CH_NUM[1].PWM_CDTY = 0b00000000000011001101000101000000;
-    PWM->PWM_ENA |= 3;
+    PWM->PWM_CLK |= (5 << 8);
+    PWM->PWM_CH_NUM[1].PWM_CMR = 0b1011;
+    PWM->PWM_CH_NUM[1].PWM_CPRD = 0b1100110100010100;
+    PWM->PWM_CH_NUM[1].PWM_CDTY = CDTY_L;
+    PWM->PWM_ENA |= 2;
+
+
 
     //Uncomment after including uart above
     uart_init(F_CPU, 9600);
@@ -46,19 +54,17 @@ int main()
     msg2.length = 8;
     Byte8 data = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
     msg2.byte8 = data;
+
     while (1)
     {
-        /* code */
-        // PIOB->PIO_SODR = PIO_SODR_P13;
-        // time_spinFor(msecs(1000));
-        // PIOB->PIO_CODR = PIO_CODR_P13;
-        // time_spinFor(msecs(1000));
 
         //can_tx(msg2);
         if (can_rx(msg))
         {
             can_printmsg(*msg);
         }
+        uint16_t cdty_value = CDTY_L - (CDTY_STEP_DIFF * msg->byte[0]);
+        PWM->PWM_CH_NUM[1].PWM_CDTYUPD = cdty_value;
 
     }
     
